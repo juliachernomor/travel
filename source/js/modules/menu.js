56a6c35d977ed5440c37ce0f695075c3c1b7fb29
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2023-08-30 06:53:10 +0300

    add some changes

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,19 +1,53 @@
+import {ScrollLock} from '../utils/scroll-lock';
+window.scrollLock = new ScrollLock();
+
+
 let navMain = document.querySelector('.nav');
 let navToggle = document.querySelector('.nav__toggle');
 let header = document.querySelector('#header');
+let navItems = document.querySelectorAll('.nav__item');
+
 
 navMain.classList.remove('nav--nojs');
+
+const closeMenu = () => {
+  navMain.classList.add('nav--closed');
+  navMain.classList.remove('nav--opened');
+  header.classList.remove('header--active');
+  window.scrollLock.enableScrolling();
+  document.removeEventListener('click', onDocumentOutside);
+};
+
+const openMenu = () => {
+  navMain.classList.remove('nav--closed');
+  navMain.classList.add('nav--opened');
+  header.classList.add('header--active');
+  window.scrollLock.disableScrolling();
+  document.addEventListener('click', onDocumentOutside);
+};
+
+const onDocumentOutside = (evt) => {
+  if (evt.target === header || header.contains(evt.target)) {
+    return;
+  } else {
+    closeMenu();
+  }
+};
+
+navItems.forEach((element) => element.addEventListener('click', (evt) => {
+  if (evt) {
+    if (navMain.classList.contains('nav--opened')) {
+      closeMenu();
+    }
+  }
+}));
 
 const effectMenu = () => {
   navToggle.addEventListener('click', function () {
     if (navMain.classList.contains('nav--closed')) {
-      navMain.classList.remove('nav--closed');
-      navMain.classList.add('nav--opened');
-      header.classList.add('header--active');
+      openMenu();
     } else {
-      navMain.classList.add('nav--closed');
-      navMain.classList.remove('nav--opened');
-      header.classList.remove('header--active');
+      closeMenu();
     }
   });
 };
